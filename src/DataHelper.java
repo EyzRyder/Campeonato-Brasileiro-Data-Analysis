7c3cdf10af76bd2de7c904203f35c3d94ae116ec
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-10-18 11:08:15 -0300

    fix: remover o removefirst pq não tava conseguindo remover por causa dos threads

--- src/DataHelper.java
+++ src/DataHelper.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class DataHelper {
     public List<Cartao> cartoes;
@@ -12,7 +10,6 @@ public class DataHelper {
     public List<Brasileirao> brasileiraoes;
 
     DataHelper() throws IOException {
-        // usar File pra carregar todos
         var rootFolder = Paths.get("./src/database/brasileirao");
         Files.list(rootFolder)
                 .filter(path -> path.toString().endsWith(".csv"))
@@ -30,7 +27,6 @@ public class DataHelper {
                                                         return new Gol(row[0], row[1], row[2], row[3], row[4]);
                                                     }
                                                 }).toList();
-                                       gols.removeFirst();
 
                                     } catch (IOException e) {
                                         throw new RuntimeException(e);
@@ -45,7 +41,6 @@ public class DataHelper {
                                                     String[] row = line.replaceAll("\"", "").split(",");
                                                         return new Brasileirao(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8], row[9], row[10],row[11], row[12], row[13], row[14], row[15]);
                                                 }).toList();
-                                        brasileiraoes.removeFirst();
 
                                     } catch (IOException e) {
                                         throw new RuntimeException(e);
@@ -60,7 +55,6 @@ public class DataHelper {
                                                     String[] row = line.replaceAll("\"", "").split(",");
                                                     return new Cartao(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7]);
                                                 }).toList();
-                                        cartoes.removeFirst();
 
                                     } catch (IOException e) {
                                         throw new RuntimeException(e);
@@ -75,7 +69,6 @@ public class DataHelper {
                                                     String[] row = line.replaceAll("\"", "").split(",");
                                                     return new Estatistica(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8], row[9], row[10],row[11], row[12]);
                                                 }).toList();
-                                       estatisticas.removeFirst();
 
                                     } catch (IOException e) {
                                         throw new RuntimeException(e);
@@ -86,10 +79,6 @@ public class DataHelper {
                         }
                 );
 
-
     }
 
-    public static void main(String[] args) throws IOException {
-        DataHelper a = new DataHelper();
-    }
 }
